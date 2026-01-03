56e56bf331003934b013d34e09df672038fe7e3d
Alireza9091 <146017988+Alireza9091@users.noreply.github.com>
2026-01-02 19:42:55 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://alireza9091.github.io',
-  base: '/northinca/',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
