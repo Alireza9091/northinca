3df44734607e1e9b146001fea7e952f26cf0a18e
Alireza9091 <146017988+Alireza9091@users.noreply.github.com>
2026-02-19 17:56:44 -0500

    Update partners.ts

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -7,6 +7,20 @@ export interface Partner {
 
 export const partners: Partner[] = [
   {
+    name: "Oceanpharma",
+    description:
+      "Oceanpharma was founded in 1978 by Christian Günther near Hamburg. His motto: We help nature to act. His goal: to develop products that … work, of course!Based on this philosophy, the company has been looking for special natural substances for your health ever since.Our oceans form an almost inexhaustible reservoir of potential new active ingredients. Oceanpharma use the power of nature for people’s health and natural appearance. To this end, they have been researching the extraordinary potential of our oceans for over four decades and producing products that work … naturally!",
+    image: "/images/partners/Inter-Cosmetiques-1.jpeg",
+    website: "#",
+  },
+  {
+    name: "Spirularin",
+    description:
+      "We believe, you need to go deeper to come up with the best solutions. Which is why we keep researching where others reach their limits. That’s how we discovered the amazing defence mechanisms of the 3.6-billion-year-old microalga Spirulina platensis and harnessed its power in the unique active agent Spiralin®. Professional foot care with active microalgae protection.  Spirularin® repairs, vitalizes and protects. Our products have been developed for the special care of problem skin, feet and nails in collaboration with dermatologists and podiatrists.",
+    image: "/images/partners/Oxygen_Botanicals.jpeg",
+    website: "#",
+  },
+  {
     name: "Inter Cosmetiques",
     description:
       "Inter Cosmetiques is a private label cosmetic manufacturing company focused on delivering exceptional skincare and personal care products to better support our clients for the global marketplace. Creates safe skincare and personal care products, Inter Cosmetiques’ R&D team is composed of highly knowledgeable researchers, chemists, scientists and technicians that develop formulations current with today’s industry standards. Formulations are perfected in our laboratories, which are designed to conduct product testing during each stage within product manufacturing. Production is carried out in Health Canada licensed facilities, adhering to strict GMP standards and environment. You can rest assured as our highly trained production team is fully supervised and documented by our QA/QC staff who observe the highest standards of quality control and product testing. We’re on the leading edge of today’s latest trends and innovations in skincare. Our boutique approach lets you hit the ground running by fulfilling new hot industry trends to get you to market faster than your competitors.",
@@ -24,6 +38,13 @@ export const partners: Partner[] = [
     name: "NOV Natural Organic Vegan",
     description:
       "NOV Natural Organic Vegan offers a range of high-performance skincare and personal care products developed with advanced formulations that combine high-quality, certified organic plant extracts — cultivated without pesticides, harmful chemicals, or contaminated soil. Each product is Vegan-certified, cruelty-free, and eco-friendly, created under sustainable production standards. Formulas are free from all harmful substances such as parabens, sulfates, silicones, and synthetic fragrances. Enriched with microbiome-supporting natural prebiotics, NOV products strengthen the skin’s natural balance and resilience. All formulations are dermatologically tested and proven to be safe even for highly sensitive or compromised skin.",
+    image: "/images/partners/Nov_log_buyuk.jpg",
+    website: "#",
+  },
+  {
+    name: "Cliniceutica",
+    description:
+      " Clinceutica Canada’s research and development teams considers as first priority to provide skin specialists with high-performing and safe dermocosmetic products adapted to the requirements of each consumer. We strive to offer users of Cliniceutica Canada brand a pleasant experience with our products because each skin is unique and requires specific needs.Expectations vary depending on each consumer who requires guaranteed efficiency as well.",
     image: "/images/partners/Nov_log_buyuk.jpg",
     website: "#",
   },
